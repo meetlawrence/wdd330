9bd5fd0e8dd8da2485d545f1d5351e72abd91df3
meetlawrence <lokon4@byupathway.edu>
2026-04-09 00:18:55 +0100

    Started developing CineVibe

--- cinevibe/scripts/modules/movie-card.js
+++ cinevibe/scripts/modules/movie-card.js
@@ -1,4 +1,3 @@
-// js/modules/movie-card.js
 export function createMovieCard(movie) {
   const posterPath = movie.poster_path 
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
@@ -6,11 +5,24 @@ export function createMovieCard(movie) {
 
   return `
     <div class="movie-card" data-id="${movie.id}">
+      <div class="trending-badge" title="Trending Now">
+        <div>
+          <svg class="icon-trending"><use href="assets/icons/sprites.svg#icon-trending"></use></svg>
+        </div>
+        <div>
+          <p>Trending</p>
+        </div> 
+      </div>
+      
       <img src="${posterPath}" alt="${movie.title}" loading="lazy">
+      
       <div class="card-info">
         <h3>${movie.title}</h3>
         <div class="meta">
-          <span class="rating"><i class="star-icon"></i> ${movie.vote_average.toFixed(1)}</span>
+          <span class="rating">
+            <svg class="icon-star"><use href="assets/icons/sprites.svg#icon-rating"></use></svg>
+            ${movie.vote_average.toFixed(1)}
+          </span>
           <span class="year">${movie.release_date?.split('-')[0] || 'N/A'}</span>
         </div>
       </div>
